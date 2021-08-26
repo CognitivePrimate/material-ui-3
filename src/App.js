79e622c56871c296f02439c919d652b536a930aa
Derek Jackson <d4jackson84@gmail.com>
2021-08-26 16:40:10 -0400

    material ui learning

--- src/App.js
+++ src/App.js
@@ -1,24 +1,80 @@
-import logo from './logo.svg';
+
 import './App.css';
 
+// importing from material ui
+import { Typography, AppBar, Card, CardActions, CardContent, CardMedia, CssBaseline, Grid, Toolbar, Container, Button, ButtonGroup } from "@material-ui/core"
+import { PhotoCamera } from "@material-ui/icons";
+
+// importing js styles from other file
+import useStyles from "./styles";
+
 function App() {
+  const classes = useStyles();
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <CssBaseline />
+      <AppBar position="relative">
+        <Toolbar >
+          <PhotoCamera className={classes.icon}/>
+          <Typography variant="h6">
+            Photo Album
+          </Typography>
+        </Toolbar>
+      </AppBar>
+      <main className={classes.container}>
+        <div>
+          <Container maxWidth="sm">
+            <Typography variant="h2" align="center" color="textPrimary" gutterBottom> {/* gutterbottom adds margin to bottom*/}
+              Photo Album 
+            </Typography>
+            <Typography variant="h6" align="center" color="textSecondary" paragraph>
+              Putting basic words into a long sentence that i'm making up on the fly to illustrate some basic functionality of Material-UI and such and such and such and also cats are awesome.
+            </Typography>
+            <div className={classes.button}>
+              <Grid container spacing={2} justify="center">
+                <Grid item>
+                  <Button variant="contained" color="primary">
+                    See my Photos
+                  </Button>
+                </Grid>
+                <Grid item>
+                  <Button variant="outlined" color="primary">
+                    Secondary Action
+                  </Button>
+                </Grid>
+
+              </Grid>
+            </div>
+          </Container>
+        </div>
+        <Container className={classes.cardGrid} maxWidth="md">
+          <Grid container spacing={4}>
+            <Grid item>
+              <Card className={classes.card}>
+                <CardMedia 
+                  className={classes.cardMedia}
+                  image="https://source.unsplash.com/random"
+                  title="Image title"
+                />
+                <CardContent classname={classes.cardContent}>
+                  <Typography gutterBottom variant="h5">
+                    Heading
+                  </Typography>
+                  <Typography>
+                    This is a media card. Use this section to describe the content.
+                  </Typography>
+                </CardContent>
+                <CardActions>
+                  <Button size="small" color="primary">View</Button>
+                  <Button size="small" color="primary">Edit</Button>
+                </CardActions>
+                
+              </Card>
+            </Grid>
+          </Grid>
+        </Container>
+      </main>
+    </>
   );
 }
 
